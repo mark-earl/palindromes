26725e54946602e834e2ac5036c731a94976fe2b
Mark P. Earl <mpe12@uakron.edu>
2023-02-01 16:34:23 -0500

    Add header comment

--- PalindromeR.hpp
+++ PalindromeR.hpp
@@ -1,16 +1,19 @@
-// palindromeR.hpp -- recursive palindrome tester
-// c. 2015 A. Deeter
+/*
+    PalindromeR.hpp
+
+    Recursive palindrome tester, interface file for the PalindromeR class
+*/
 
 #ifndef PALINDROMER_HPP
 #define PALINDROMER_HPP
 
 #include <string>
 
-class PalindromeR {
-
-public:
-  PalindromeR() {}
-  int test_string(std::string s);
+class PalindromeR
+{
+    public:
+        PalindromeR() {}
+        int test_string(std::string s);
 };
 
 #endif
